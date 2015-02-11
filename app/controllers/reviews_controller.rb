b121b8edd0c96cf5caba4a21ee04f230b0d7a9a0
neil patel <neil@myindianweddingapp.com>
2015-02-11 11:33:07 +0000

    Fixed Bug in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,10 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
 
+
   respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
 
   def new
     @review = Review.new
@@ -24,8 +17,14 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    @review.save
-    respond_with(@review)
+    @review.movie_id = @movie.id
+
+    if @review.save
+    redirect_to @movie
+    else
+    render 'new'
+    end 
+
   end
 
   def update
@@ -42,6 +41,11 @@ class ReviewsController < ApplicationController
     def set_review
       @review = Review.find(params[:id])
     end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+
 
     def review_params
       params.require(:review).permit(:rating, :comment)
